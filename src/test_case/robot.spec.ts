34a20725cb0ce261b4c2d64c1fb9a6e26e855194
ashishbokaro <149461440+ashishbokaro@users.noreply.github.com>
2023-11-02 17:49:20 +0530

    Add files via upload

--- src/test_case/robot.spec.ts
+++ src/test_case/robot.spec.ts
@@ -14,13 +14,20 @@ describe('Robot', () => {
       { position: 'S 0 0', commands: 'M1RM4L3M3', expected: 'N 96 2' },
       { position: 'E 0 0', commands: 'M1RM4LR13M3', expected: 'S 1 93' },
       { position: 'W 10 12', commands: 'M1RM4LR13M3', expected: 'N 9 19' },
+      { position: 'E 10 10', commands: 'M1R4M5', expected: 'E 16 10' },
+      { position: 'N 10 10', commands: 'M1R4M1', expected: 'N 10 12' },
+      { position: 'S 10 10', commands: 'M1R4M5L1', expected: 'E 10 4' },
+      { position: 'N 0 10', commands: 'M1R1M5', expected: 'E 5 11' },
+      { position: 'W 0 10', commands: 'M1R2M5L1M4', expected: 'N 4 14' },
+      { position: 'E 0 10', commands: 'M1R4M8', expected: 'E 9 10' },
+      { position: 'S 0 10', commands: 'M1R3M4R2M6L1', expected: 'S 98 9' },
     ];
 
     testCases.forEach((testCase) => {
       const robot = new Robot(widthSpace, heightSpace)
       const result = robot.executeCommands(testCase.position, testCase.commands);
 
-      console.log('basic commands correctly', result)
+      console.log('basic commands correctly', testCase.position, testCase.commands, result)
       expect(result).to.equal(testCase.expected);
     });
   });
